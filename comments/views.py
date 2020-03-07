e2541235caef6c4d98611e67c17ec4dc2b21044b
hih-pop <1466758260@qq.com>
2020-03-07 14:30:38 +0800

    初始化

---
+++ comments/views.py
@@ -0,0 +1,28 @@
+from blog.models import Post
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
+
+from .forms import CommentForm
+from django.contrib import messages
+
+
+@require_POST
+def comment(request, post_pk):
+    # 先获取被评论的文章，因为后面需要把评论和被评论的文章关联起来。
+    # 这里我们使用了 django 提供的一个快捷函数 get_object_or_404，
+    # 这个函数的作用是当获取的文章（Post）存在时，则获取；否则返回 404 页面给用户。
+    post = get_object_or_404(Post, pk=post_pk)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+        messages.add_message(request, messages.SUCCESS, '评论发表成功！', extra_tags='success')
+        return redirect(post)
+    context = {
+        'post': post,
+        'form': form,
+    }
+    messages.add_message(request, messages.ERROR, '评论发表失败！请修改表单中的错误后重新提交。', extra_tags='danger')
+    return render(request, 'comments/preview.html', context=context)
