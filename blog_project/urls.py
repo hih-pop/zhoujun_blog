23e63da9bde29f2c8cb26668e169c88307db3f57
hih-pop <1466758260@qq.com>
2020-03-07 18:31:44 +0800

    change admin url 02

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'admin/$', admin.site.urls),
+    path(r'admin', admin.site.urls),
     path(r'', include('blog.urls')),
     path(r'', include('comments.urls'))
 ]
