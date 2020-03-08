3015c9abf4ad34c5f125c43f1810abe454b067d9
hih-pop <1466758260@qq.com>
2020-03-09 01:52:09 +0800

    set fabric

--- fabfile.py
+++ fabfile.py
@@ -23,17 +23,23 @@ def deploy(c):
     supervisor_conf_path = '~/etc/'
     supervisor_program_name = 'zhoujun_blog'
 
-    project_root_path = '~/apps/zhoujun_blog'
+    project_root_path = '~/apps/zhoujun_blog/'
 
     # 先停止应用
     with c.cd(supervisor_conf_path):
         cmd = f'supervisorctl stop {supervisor_program_name}'
 
     # 进入项目根目录，从git拉去新代码
-    with c.cd(supervisor_conf_path):
+    with c.cd(project_root_path):
         cmd = 'git pull'
         responders = _get_github_auth_responders()
         c.run(cmd, watchers=responders)
+
+    # 安装依赖，迁移数据库，收集静态文件
+    with c.cd(project_root_path):
+        c.run('pip3 install -r requirements.txt')
+        c.run('python manage.py migrate')
+        c.run('python collectstatic --noinput')
 
     # 重新启动应用
     with c.cd(supervisor_conf_path):
